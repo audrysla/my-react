2666fdd59b0b6f312b07d2eb533e9ea07a111e95
audry <kmkyou1@nate.com>
2023-09-24 20:26:37 +0900

    파일 정리

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Tabs from './m/Tabs';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-const tabs = ReactDOM.createRoot(document.getElementById('Tabs'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-tabs.render(
+root.render(
   <React.StrictMode>
-    <Tabs />
+    <App />
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
