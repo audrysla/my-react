9e3f7ed504d690289206306fa332760979446610
audry <kmkyou1@nate.com>
2023-09-26 20:08:10 +0900

    상품 정리

--- src/constants/Products.jsx
+++ src/constants/Products.jsx
@@ -4,16 +4,16 @@ export const Products = [
     type : 'today_best',
     img : 'https://img-cf.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/shop/data/goods/1650353599366l0.jpg',
     title : '[바나나] 탄력 3종 세트 (쇼핑백+쿠폰19760원+적립6612원)',
-    price : '190,0000원',
+    price : '190,000원',
     dimmedPrice : '290,000원',
     discount : '20%'
   },
   {
     id : 22,
     type : 'today_best',
-    img : 'https://img-cf.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/shop/data/goods/1538466010000_l.jpg',
+    img : 'https://img-cf.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/shop/data/goods/1513759007134l0.jpg',
     title : '미쏘 본사 15%즉할+10%쿠폰 가을 아우터 대전 자켓/코트/패딩/이너 등 421종',
-    price : '190,0000원',
+    price : '190,000원',
     dimmedPrice : '39,900원',
     discount : '50%'
   },
@@ -40,8 +40,8 @@ export const Products = [
     type : 'today_best',
     img : 'https://3p-image.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/files/cc1ac6f0-81f6-473c-899d-edf544e3ea7a/ce1b6e83-1cda-472a-b17f-4c7c1aa11cfe.jpg',
     title : '생어거스틴 6만원 자유이용권 40% 할인 프로모션',
-    price : '190,0000원',
-    dimmedPrice : '290,000원',
+    price : '60,000원',
+    dimmedPrice : '110,000원',
     discount : '40%'
   },
   {
@@ -49,8 +49,8 @@ export const Products = [
     type : 'today_best',
     img : 'https://product-image.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/product/image/022b8205-dd18-4692-b13a-d362d91d93e5.jpg',
     title : '이디야커피 더욱 풍부해진 리얼 토피넛 신메뉴 20% 할인',
-    price : '190,0000원',
-    dimmedPrice : '290,000원',
+    price : '15,500원',
+    dimmedPrice : '45,300원',
     discount : '20%'
   },
   {
@@ -58,8 +58,8 @@ export const Products = [
     type : 'category_best',
     img : 'https://img-cf.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/shop/data/goods/1553162235402l0.jpg',
     title : '사료·간식 박스',
-    price : '190,0000원',
-    dimmedPrice : '290,000원',
+    price : '45,000원',
+    dimmedPrice : '50,000원',
     discount : '20%'
   },
   {
@@ -67,8 +67,8 @@ export const Products = [
     type : 'category_best',
     img : 'https://3p-image.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/product-image/bd1ae1d1-5102-4e2c-9fa8-ce19098f9248/49ccd0af-bb39-4f33-bfac-8c2c1b9fc01b.jpg',
     title : '외출·리빙',
-    price : '190,0000원',
-    dimmedPrice : '290,000원',
+    price : '67,400원',
+    dimmedPrice : '132,000원',
     discount : '20%'
   },
   {
@@ -76,8 +76,8 @@ export const Products = [
     type : 'category_best',
     img : 'https://product-image.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/product/image/2e293dee-f448-40b6-930f-577a9732fd40.jpg',
     title : '미용·건강',
-    price : '190,0000원',
-    dimmedPrice : '290,000원',
+    price : '35,000원',
+    dimmedPrice : '75,000원',
     discount : '20%'
   },
   {
@@ -85,8 +85,8 @@ export const Products = [
     type : 'category_best',
     img : 'https://product-image.kurly.com/cdn-cgi/image/fit=crop,width=360,height=468,quality=85/product/image/fc3eb710-126d-4c18-b776-ac6809b07a73.jpeg',
     title : '정육·과일',
-    price : '190,0000원',
-    dimmedPrice : '290,000원',
+    price : '45,000원',
+    dimmedPrice : '50,000원',
     discount : '20%'
   },
 ];
