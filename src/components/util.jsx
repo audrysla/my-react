62c412734e99288c71634691840c7fb9568b0384
audry <kmkyou1@nate.com>
2023-10-04 18:22:54 +0900

    애니메이션 제거

--- src/components/util.jsx
+++ src/components/util.jsx
@@ -7,20 +7,20 @@ font-family: 'Noto Sans KR', sans-serif;
   &:nth-child(2){
     position:relative;
     &::after{
-      // content:"6";
-      // color:#fff;
-      // background:#e93838;
-      // display:block;
-      // width:22px;
-      // height:22px;
-      // line-height: 20px;
-      // font-size:15px;
-      // border-radius:100%;
-      // text-align:center;
-      // position: absolute;
-      // top: -8px;
-      // right: -8px;
-      // font-weight:600;
+      content:"6";
+      color:#fff;
+      background:#e93838;
+      display:block;
+      width:22px;
+      height:22px;
+      line-height: 20px;
+      font-size:15px;
+      border-radius:100%;
+      text-align:center;
+      position: absolute;
+      top: -8px;
+      right: -8px;
+      font-weight:600;
     }
   }
 `;
