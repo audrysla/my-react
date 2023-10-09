318083b50ff83c31259d876a4babba685519ff78
audry <kmkyou1@nate.com>
2023-10-10 00:42:42 +0900

    알람 애니메이션 적용

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import styled from "styled-components";
 import Tab from '../components/tab/Tab';
@@ -28,24 +28,46 @@ const H1 = styled.h1`
   margin-bottom:15px;
 `;
 const Warp = styled.div``;
+const Elem = styled.div`
+  position:fixed;
+  z-index:99999;
+  top:700px;
+  left:-10%;
+`;
+
 function Contents(){
+  // const Dom = useRef(null);
+  // const handleScroll = (e) =>{
+  //   // console.log(window.scrollY)
+  //   // console.log(Dom.current.offsetLeft)
+  //   let domLeft = Dom.current.offsetLeft;
+  //   Dom.current.style.setProperty('left', `${window.scrollY}px`);
+  // }
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll, { capture: true }); // 스크롤 이벤트 등록
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll); 		// 스크롤 이벤트 제거
+  //   };
+  // }, []);
+
   return (
     <Warp> 
       <Header/>
       <div id="contents">
         {/* <MainBn/> */}
-        <div className='inner'>        
-            <Container className='container'>
-              <SwiperComponent/>
-              <PC><Tab/></PC>
-            </Container>
-            <MainProduct>
-              <PC><H1>추천 상품 404</H1></PC>
-              <div className="banner bn1">추석 선물</div>
-            </MainProduct> 
+        <div className='inner'>
+          <Container className='container'>
+            <SwiperComponent/>
+            <PC><Tab/></PC>
+          </Container>
+          <MainProduct>
+            <PC><H1>추천 상품 404</H1></PC>
+            <div className="banner bn1">추석 선물</div>
+          </MainProduct> 
           
           <ProductList />
           <div className="banner bn2">추석 선물</div>
+          {/* <Elem className='dom' ref={Dom}>아이콘</Elem> */}
         </div>
       </div>
     </Warp> 
